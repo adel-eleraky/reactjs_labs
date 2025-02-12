798c957d1f961a25c8878add67eb1dbaa1b902c9
el3raky <kameladel339@gmail.com>
2025-02-12 21:52:13 +0200

    feat: display cart length

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router'
+import { UserContext } from '../context/UserContext'
+import { CartContext } from '../context/CartContext'
 
 function NavBar() {
 
-    let user = JSON.parse(localStorage.getItem("user")) || null
-    console.log(user)
+    let { user, setUser } = useContext(UserContext)
+    let { cart, setCart } = useContext(CartContext)
+
+    console.log(cart)
+    async function logout() {
+        localStorage.clear("user")
+        setUser(null)
+    }
+
     return (
-        <nav className="navbar navbar-expand-lg " style={{ backgroundColor: "rgb(5 72 25 / 52%) !important" }}>
+        <nav className="navbar navbar-expand-lg " style={{ backgroundColor: "rgb(5 72 25 / 52%)" }}>
             <div className="container-fluid ">
                 <Link className="navbar-brand text-white" to="/">Home</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,9 +27,20 @@ function NavBar() {
                             <Link className="nav-link active text-white" aria-current="page" to="categories">Categories</Link>
                         </li>
                         {user ?
-                            <li className="nav-item">
-                                <Link className="nav-link text-white" to="account">{ user?.data?.name }</Link>
-                            </li>
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link text-white" to="account">{user?.data?.name}</Link>
+                                </li>
+                                <li className="nav-item position-relative">
+                                    <Link className="nav-link text-white" to="cart">
+                                        <i className="fa-solid fs-4 fa-cart-plus me-2"></i>
+                                        <span className='badge bg-dark position-absolute top-0' style={{ right: "5px"}}> {cart.length}</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-white" onClick={logout}>Logout</a>
+                                </li>
+                            </>
                             :
                             <>
                                 <li className="nav-item">
