bed76897ead6a5c79a8e14a80431f70660a6b694
el3raky <kameladel339@gmail.com>
2025-02-13 13:26:31 +0200

    feat: displayed items in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,24 +1,24 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { CartContext } from '../context/CartContext'
-import { renderProducts } from '../utils/Products'
 import axios from 'axios'
+import "./css/Cart.css"
 
 function Cart() {
 
     const { cart } = useContext(CartContext)
 
-    let [products, setProducts] = useState()
+    let [products, setProducts] = useState([])
 
+    console.log(cart)
     useEffect(() => {
         cart.map(item => {
-            console.log(item)
             fetchProduct(item.productId).then(data => {
-                // products.push(data)
-                setProducts(data)
+                setProducts(prev => {
+                    if (prev.some(item => item.id == data.id)) return prev
+                    return [...prev, data]
+                })
             })
         })
-
-        // console.log(products)
     }, [cart])
 
     async function fetchProduct(id) {
@@ -28,16 +28,72 @@ function Cart() {
         } catch (err) {
             console.log(err)
         }
-
     }
 
-    console.log(products)
-    let productsElements = products && renderProducts([products])
+
+    let productsElements = products && renderCartItems(products)
+
+    function renderCartItems(items) {
+        let qty = 1
+        return items.map(item => {
+            qty = cart.find(i => i.productId == item.id).quantity
+            return (
+                <tr>
+                    <td className='d-flex align-items-center py-4'>
+                        <i className="fa-regular fa-circle-xmark fs-4" ></i>
+
+                        <img alt="" src={item.image} className="img-fluid product-img d-block mx-3 rounded"
+                            style={{ backgroundColor: "#e5e5e5", width: "40px" }} />
+                        <a className='text-dark title'>
+                            <p className="m-0">{item.title.substring(0, 20) + "..."} </p>
+                        </a>
+                    </td>
+                    <td className="price"> {item.price} </td>
+                    <td className="qty">
+                        <div className="d-flex justify-content-center mt-3">
+                            <button className="btn btn-success px-3 py-1 rounded" >+</button>
+                            <input className="p-0 outline-0 mx-1 rounded qty-input" value={qty} type="number" name="quantity" />
+                            <button className="btn btn-danger px-3 py-1 rounded" >-</button>
+                        </div>
+                    </td>
+                    <td className="total-price">{item.price * 1} </td>
+                </tr>
+            )
+        })
+    }
+
     return (
-        <div className='container py-3'>
-            <div className="row">
-                {productsElements}
-            </div>
+        <div className="cart-page">
+            <div className='container py-3'>
+                <div className="row">
+                    <div className='col-12 col-lg-8 table-container'>
+                        <table className="table text-center">
+                            <thead>
+                                <tr>
+                                    <th scope="col" className='w-50'>Product</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {productsElements}
+
+                            </tbody >
+                        </table >
+                    </div >
+                    <div className="col-12 col-lg-4">
+                        <div className='cart-total'>
+                            <h3 className='mb-3'>Total Price : cart.totalPrice </h3>
+                            <div className="coupon-card">
+                                <input className='coupon-input' type='text' name='coupon' placeholder='coupon code' />
+                                <button className="btn coupon-btn text-white">Apply Coupon</button>
+                            </div>
+                            <button className="btn text-white checkout-btn">Proceed to Check Out</button>
+                        </div>
+                    </div>
+                </div >
+            </div >
         </div>
     )
 }
