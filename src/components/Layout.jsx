7c955d592ad312a3d724d9fd98615610e5ebfbf2
el3raky <kameladel339@gmail.com>
2025-02-24 21:20:36 +0200

    added toastify

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router'
+import { ToastContainer } from 'react-toastify';
+import { getLoggedInUser } from '../rtk/features/AuthSlice';
+import { useDispatch } from 'react-redux';
 
 function Layout() {
+
+    const dispatch = useDispatch()
+    let [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        // dispatch(f())
+        dispatch(getLoggedInUser()).then(() => {
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) {
+        return (
+            // <div className='vh-100 d-flex justify-content-center align-items-center'>
+            //     <RotateSpinner size={100} color="#55c57a" loading={loading} />
+            // </div>
+            <div>loading</div>
+        )
+    }
 
     return (
         <>
             <NavBar />
             <Outlet />
+            <ToastContainer />
         </>
     )
 }
